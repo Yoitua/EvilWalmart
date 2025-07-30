3eb427803d593db9710492a8f4c79842b1f33c5e
Yoitua <123353177+Yoitua@users.noreply.github.com>
2025-07-29 19:20:47 -0700

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,13 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-     host : gh46.glacierhosting.org
-port : 25583
-pass : youshallnotpass
-source : false
+    {
+      name: "Anakin",
+      password: "anakin",
+      host: "us.apsara.lol",
+      port:  25575,
+      secure: false
+    }
     }
   ]
 }
